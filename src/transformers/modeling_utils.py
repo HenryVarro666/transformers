8cdb4fbb3138c5e1ebc8c070d5fa2fd12871da43
3outeille <ferdinand.mom@epita.fr>
2026-07-14 04:02:11 +0000

    add weight loading logging

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -4446,6 +4446,7 @@ class PreTrainedModel(
                 unexpected_keys=set(),
                 mismatched_keys=set(),
                 conversion_errors={},
+                skipped_pp_keys=set(),
             )
         else:
             all_pointer = set()
